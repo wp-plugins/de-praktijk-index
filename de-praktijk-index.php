672f3ebed630281945f909366890fa6d29975b7b
leonhooijer <leonhooijer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-08 21:15:44 +0000

    Adding first version of the plugin
    git-svn-id: https://plugins.svn.wordpress.org/de-praktijk-index/trunk@817099 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ de-praktijk-index.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Plugin Name: De Praktijk Index
+ * Plugin URI: http://www.depraktijkindex.nl
+ * Description: Plugin for De Praktijk Index, Bilthoven.
+ * Version: 0.2.1
+ * Author: Leon Hooijer
+ * Author URI: http://www.leonhooijer.nl
+ * License: GPL2
+ */
+ /*  Copyright 2013  Leon Hooijer  (email : leonhooijer@hotmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+/* Add sortable Last Modified column */
+function last_modified_sort($columns) {
+		$columns['column-modified'] = 'modified';
+		return $columns;
+}
+
+add_filter("manage_edit-home_slider_sortable_columns", 'last_modified_sort');
+add_filter("manage_edit-portfolio_sortable_columns", 'last_modified_sort');
+add_filter("manage_edit-nectar_slider_sortable_columns", 'last_modified_sort');
+
+$args = array(
+   'public'   => true,
+);
+$output = 'names'; // names or objects, note names is the default
+$operator = 'and'; // 'and' or 'or'
+$post_types = get_post_types( $args, $output, $operator ); 
+foreach ( $post_types as $post_type ) {
+   add_filter("manage_edit-".$post_type."_sortable_columns", 'last_modified_sort');
+}
+/* End Of: Add sortable Last Modified column */
---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== De Praktijk Index ===
+Contributors: Leon Hooijer,
+Tags: De Praktijk Index, Bilthoven, Real Time Monitoring, HSMR
+Donate link: http://www.depraktijkindex.nl
+Requires at least: 3.6
+Tested up to: 3.7.1
+Stable tag: 0.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Plugin for De Praktijk Index, Bilthoven.
+
+== Description ==
+Plugin for De Praktijk Index, Bilthoven.
+
+== Changelog ==
+= 0.1 =
+* Initial release.
+
+= 0.2 =
+* Added sortable "Last Modfied" column to all edit overviews (It checks if Salient is active)
+
+== Upgrade Notice ==
+= 0.1 =
+* Initial release.
+
+= 0.2 =
+* Added sortable "Last Modfied" column to all edit overviews (It checks if Salient is active)
